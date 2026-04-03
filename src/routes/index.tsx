20ceed843197a16179c03fd03ddff032d4ff4dae
Chisom Udonsi <udonsichisom02@gmail.com>
2026-04-03 13:24:06 +0100

    feature(home): settle perks UI

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,16 @@
 import { createFileRoute } from "@tanstack/react-router";
 import AboutSection from "#/components/about-section";
 import HeroSection from "#/components/hero-section";
+import PerksSection from "#/components/perks-section";
 
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
-	return (
-		<main>
-			<HeroSection />
-			<AboutSection />
-		</main>
-	);
+  return (
+    <main>
+      <HeroSection />
+      <AboutSection />
+      <PerksSection />
+    </main>
+  );
 }
